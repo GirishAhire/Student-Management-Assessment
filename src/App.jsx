240572a7050aa827a22162ce1a41b84b9d665d46
GirishAhire <117669689+GirishAhire@users.noreply.github.com>
2025-06-11 18:39:53 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import StudentForm from "./component/StudentForm"
+import StudentTable from "./component/StudentTable"
+
+function App() {
+
+  return (
+    <>
+    <StudentForm />
+    
+    <StudentTable />
+     
+    </>
+  )
+}
+
+export default App
